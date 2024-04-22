10843e5a9593dc62ecd012296f7510efbeaec3c5
Cypher-RJ <160129604+Cypher-RJ@users.noreply.github.com>
2024-04-22 19:32:11 +0100

    Add files via upload

---
+++ libft/libft/ft_strrchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcesar-d <rcesar-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 19:34:31 by rcesar-d          #+#    #+#             */
+/*   Updated: 2024/04/17 18:42:14 by rcesar-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
+
+/* #include <stdio.h>
+int main()
+{
+	const char *str = "This is a test string.";
+	int character = 'i';
+
+	char *result = ft_strrchr(str, character);
+
+	if (result == NULL)
+		printf("Character '%c' not found in string.\n", character);
+	else
+		printf("Last character '%c' found at position %ld.\n", 
+			character, result - str);
+} */
---
+++ libft/libft/ft_strtrim.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcesar-d <rcesar-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 19:37:51 by rcesar-d          #+#    #+#             */
+/*   Updated: 2024/04/18 17:53:30 by rcesar-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+	size_t	strlen;
+	char	*str;
+
+	i = 0;
+	strlen = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[i] && ft_strchr(set, s1[i]) != NULL)
+		i++;
+	j = ft_strlen(s1);
+	while (j > i && ft_strrchr(set, s1[j - 1]) != NULL)
+		j--;
+	str = malloc(sizeof(char) * (j - i + 1));
+	if (!str)
+		return (NULL);
+	while (i < j)
+	{
+		str[strlen] = s1[i];
+		strlen++;
+		i++;
+	}
+	str[strlen] = '\0';
+	return (str);
+}
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+// Assuming ft_strtrim is declared in another file or above in this file
+char *ft_strtrim(char const *s1, char const *set);
+
+int main() {
+    char *result;
+
+    // Test Case 1: Trimming from both ends
+    result = ft_strtrim("  hello world  ", " ");
+    printf("Test 1 - Expected: 'hello world', Got: '%s'\n", result);
+    free(result); // Remember to free the memory
+
+    // Test Case 2: Trimming from the beginning only
+    result = ft_strtrim("+++hello world", "+");
+    printf("Test 2 - Expected: 'hello world', Got: '%s'\n", result);
+    free(result);
+
+    // Test Case 3: Trimming from the end only
+    result = ft_strtrim("hello world---", "-");
+    printf("Test 3 - Expected: 'hello world', Got: '%s'\n", result);
+    free(result);
+
+    // Test Case 4: No trimming needed
+    result = ft_strtrim("hello world", "+");
+    printf("Test 4 - Expected: 'hello world', Got: '%s'\n", result);
+    free(result);
+
+    // Test Case 5: Empty string input
+    result = ft_strtrim("", " ");
+    printf("Test 5 - Expected: '', Got: '%s'\n", result);
+    free(result);
+
+    // Test Case 6: Set contains no relevant characters
+    result = ft_strtrim("hello world", "x");
+    printf("Test 6 - Expected: 'hello world', Got: '%s'\n", result);
+    free(result);
+
+    // Test Case 7: Null inputs
+    result = ft_strtrim(NULL, " ");
+    printf("Test 7 - Expected: 'NULL', Got: '%s'\n", result ? result : "NULL");
+
+    // Test Case 8: All characters are trimmed
+    result = ft_strtrim("+++++", "+");
+    printf("Test 8 - Expected: '', Got: '%s'\n", result);
+    free(result);
+
+    return 0;
+} */
